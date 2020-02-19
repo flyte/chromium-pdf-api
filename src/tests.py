9539bf4eddfac374c2a3651d02f6bf8ba63a568f
Ellis Percival <flyte@failcode.co.uk>
2020-02-19 16:05:11 +0000

    Add websocket server framework to tests

--- src/tests.py
+++ src/tests.py
@@ -1,10 +1,63 @@
 import asyncio
 from threading import Thread
+from time import sleep
 from unittest.mock import Mock, patch
 
 import pytest
+import websockets
 from pdf import chrome_ok
 from werkzeug.wrappers import Response
+
+
+async def hello(websocket, path):
+    try:
+        while True:
+            name = await websocket.recv()
+            greeting = f"Hello {name}"
+            await websocket.send(greeting)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+
+
+@pytest.yield_fixture
+def websocket_server():
+    loop = asyncio.new_event_loop()
+    server = websockets.serve(hello, "localhost", 8765, loop=loop)
+
+    def run():
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(server)
+        loop.run_forever()
+
+    async def await_all():
+        await asyncio.gather(
+            *[
+                t
+                for t in asyncio.Task.all_tasks(loop=loop)
+                if t != asyncio.Task.current_task(loop=loop)
+            ],
+            return_exceptions=True,
+        )
+
+    thread = Thread(target=run)
+    thread.start()
+    # Wait for the server to start listening
+    while True:
+        try:
+            server.ws_server.sockets
+            break
+        except AttributeError:
+            sleep(0.1)
+    host, port = server.ws_server.sockets[0].getsockname()
+    uri = f"ws://{host}:{port}"
+    yield server.ws_server, uri
+    loop.call_soon_threadsafe(server.ws_server.close())
+    loop.stop()
+    while loop.is_running():
+        sleep(0.1)
+    loop.run_until_complete(await_all())
+    loop.close()
+    thread.join()
 
 
 @pytest.mark.asyncio
@@ -51,3 +104,13 @@ async def test_chrome_ok(httpserver):
         ]
     )
     assert await chrome_ok(f"http://{httpserver.host}:{httpserver.port}") == None
+
+
+@pytest.mark.asyncio
+async def test_ws_hello(websocket_server):
+    ws_server, uri = websocket_server
+    assert ws_server.is_serving
+    async with websockets.connect(uri) as ws:
+        await ws.send("flyte")
+        resp = await ws.recv()
+    assert resp == "Hello flyte"
